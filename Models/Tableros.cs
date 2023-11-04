a6a487e6caee6b21760408ddc4e95a6ba3985e42
Cordoba Leandro <cordobaleandro000@gmail.com>
2023-11-03 22:47:29 -0300

    first commit

---
+++ Models/Tableros.cs
@@ -0,0 +1,8 @@
+namespace Tableros{
+    public class Tablero{
+        public int Id { get => Id; set => Id = value; }
+        public int IdUsuarioPropietario { get => IdUsuarioPropietario; set => IdUsuarioPropietario = value; }
+        public string? Nombre { get => Nombre; set => Nombre = value; }
+        public string? Descripcion { get => Descripcion; set => Descripcion = value; }
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,6 @@
+namespace Usuarios{
+    public class Usuario{
+        public int Id { get => Id; set => Id = value; }
+        public string? NombreDeUsuario { get => NombreDeUsuario; set => NombreDeUsuario = value; }
+    }
+}
